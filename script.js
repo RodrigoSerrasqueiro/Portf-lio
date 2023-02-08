efe9ab1fe538941a670723d76362bb972304b0f4
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-02-08 10:30:54 -0300

    Feature responsive menu

--- script.js
+++ script.js
@@ -23,5 +23,17 @@ document.getElementById("input-fone").addEventListener("keypress", function(evt)
       evt.preventDefault();
 });
 
+const Menu = document.querySelector('.menu')
+const Bar = document.querySelector('.nav-menu')
 
+Menu.addEventListener('click', () => {
+  Menu.classList.toggle('ativo')
+  Bar.classList.toggle('ativo')
+})
+document.querySelectorAll('a').forEach(a => {
+  a.addEventListener('click', () => {
+    Menu.classList.remove('ativo');
+    Bar.classList.remove('ativo');
+  });
+});
 
